2a6ed0817f6dc92304218bc1074b1347eb216f4c
Vladimir <vladimir@MacBookPro.local>
2022-07-12 08:54:08 -0500

    first commit

---
+++ api/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Safi;
+
+Console.WriteLine("Hello, World!");
+
+Repository.Client cliente = new("");
+cliente.get('resume')
+    .add(ClientID)
+
---
+++ api/Repository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+namespace Safi
+{
+    public class Repository
+    {   public interface IRepository
+        {
+            void resume();
+        }
+
+        public Repository()
+        {
+            string instance_object = this.GetType().Name.ToLower();
+
+            string dir = Directory.GetCurrentDirectory() + "/data/" + instance_object ;
+            //file_name=
+            Console.WriteLine(instance_object);
+            // add and map all json request/json files
+            _path.Add("resume", dir + "/resume.json");
+            _path.Add("detail", dir + "/detail.json");
+
+
+            foreach (KeyValuePair<string, string> entry in _path)
+            {
+                Console.WriteLine(entry.Value);
+                if (!File.Exists(entry.Value))
+                {
+                    _path.Remove(entry.Key); 
+                }
+
+            }
+
+            string public resume
+
+
+
+
+
+
+        }
+        private Dictionary<string, string> _path = new Dictionary<string, string>();
+
+
+
+
+        public class Client :  Repository 
+        {
+
+            public Client(string request):base()
+            {
+                
+
+            }
+        }
+
+
+        public class Account : Repository
+        {
+
+            public Account(string request) : base()
+            {
+
+
+            }
+        }
+
+
+
+    }
+}
+
